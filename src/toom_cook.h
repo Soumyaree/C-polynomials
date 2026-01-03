dd82a9a670d4bbfab748e1fe46d3c3005b22d2df
Soumyasre <soumyasreechakraborty9@gmail.com>
2026-01-03 18:49:10 +0100

    delted: the enum added the base algo.

--- src/toom_cook.h
+++ src/toom_cook.h
@@ -3,14 +3,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "naive.h"
+#include "base_algo.h"
 
-typedef enum {
-    BASE_NAIVE = 0,
-    BASE_KARATSUBA = 1
-} base_algo;
-
-double* toom_cook_polynomial_multiplication(double* A, int degA, double* B, int degB, int k, base_algo base);
+double* toom_cook_polynomial_multiplication(
+    double* A, int degA,
+    double* B, int degB,
+    int k,
+    base_algo base
+);
 
 #endif
